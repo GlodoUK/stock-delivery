149c5034fc29137a51eff3a237cd01ab5ca855e5
Karl Southern <karl@theangryangel.co.uk>
2022-02-01 14:16:16 +0000

    fix(delivery_carrier_calendar): should also take into account lead_time if there is no calendar

--- delivery_carrier_calendar/models/delivery_carrier.py
+++ delivery_carrier_calendar/models/delivery_carrier.py
@@ -14,19 +14,30 @@ class DeliveryCarrier(models.Model):
     )
 
     def plan_days(
-        self, day_dt, days=1, compute_leaves=True, domain=None, safety_lead_days=1
+        self,
+        day_dt,
+        days=1,
+        compute_leaves=True,
+        domain=None,
+        safety_lead_days=1
     ):
         self.ensure_one()
 
-        if not self.carrier_calendar_id:
-            return False
+        lead_days = 0
 
-        if safety_lead_days > 0:
-            day_dt = day_dt + relativedelta(days=safety_lead_days)
+        if safety_lead_days:
+            lead_days += safety_lead_days
 
-        if self.lead_time > 0:
-            day_dt = day_dt + relativedelta(days=self.lead_time)
+        if self.lead_time:
+            lead_days += self.lead_time
 
-        return self.carrier_calendar_id.plan_days(
-            days, day_dt, compute_leaves=compute_leaves, domain=domain
-        )
+        day_dt = day_dt + relativedelta(days=lead_days)
+
+        if self.carrier_calendar_id:
+            return self.carrier_calendar_id.plan_days(
+                days, day_dt, compute_leaves=compute_leaves, domain=domain
+            )
+
+        return day_dt
+
+
