1d021d50dc90ab460ff2068da8367e542cc358bf
Karl Southern <karl@theangryangel.co.uk>
2022-05-25 14:33:41 +0100

    feat(stock_picking_move_form): add a small stock_picking_move_form ux module

--- product_commingle_mrp/__manifest__.py
+++ product_commingle_mrp/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "product_commingle_mrp",
     "summary": """product_commingle <-> mrp glue module""",
     "author": "Glo Networks",
-    "website": "https://github.com/OCA/stock-delivery",
+    "website": "https://github.com/GlodoUK/stock-delivery",
     "category": "Uncategorized",
     "version": "15.0.1.0.0",
     "depends": ["product_commingle", "mrp"],
