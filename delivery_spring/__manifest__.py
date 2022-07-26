28f988d629c36d280c0c2a380b1b370964a658f5
ellbristow <elliott@glo.systems>
2022-07-26 16:45:45 +0200

    Add delivery_spring module

---
+++ delivery_spring/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "delivery_spring",
+    "summary": """Connector to integrate with Spring courier""",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/stock-delivery",
+    "category": "Sales",
+    "version": "15.0.1.0.0",
+    "depends": ["delivery", "delivery_state_events"],
+    "data": [
+        "views/views.xml",
+        "data/data.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+    "license": "AGPL-3",
+}
