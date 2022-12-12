11c2b1a2317fdb7e5f320587099e84eb3c88562d
cshardey <cshardeynartey@gmail.com>
2022-12-12 12:34:38 +0000

    [FIX] sale_order_alternative_product: Fix ir config access

--- sale_order_alternative_products/models/sale_order_line.py
+++ sale_order_alternative_products/models/sale_order_line.py
@@ -42,7 +42,7 @@ class SaleOrderLine(models.Model):
                 record.alternatives = [(6, 0, record.product_id.alternative_ids.ids)]
 
     def check_alternative_product_setting(self):
-        config = self.env["ir.config_parameter"].get_param(
+        config = self.env["ir.config_parameter"].sudo().get_param(
             "sale_order_alternative_products.alternative_product"
         )
 
