fdc096e4ddbc9917c038cd0a6e1ff31da78d19a5
cshardey <cshardeynartey@gmail.com>
2023-02-07 18:08:47 +0000

    [FIX] show note on stock move line using related field

--- inventory_adjustment_reason/models/stock_quant.py
+++ inventory_adjustment_reason/models/stock_quant.py
@@ -1,35 +1,46 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockQuant(models.Model):
     _inherit = "stock.quant"
     note = fields.Text(string="Inventory Adjustment Reason")
 
-    def _get_inventory_fields_write(self):
-        res = super()._get_inventory_fields_write()
-        res.extend(["note"])
-        return res
-
     def _get_inventory_move_values(self, qty, location_id, location_dest_id, out=False):
-        """Function to super _get_inventory_move_values"""
         res = super()._get_inventory_move_values(
-            qty, location_id, location_dest_id, out
+            qty, location_id, location_dest_id, out=out
         )
-        move_line = res["move_line_ids"]
-        move_line[0][2]["note"] = self.note
-        res.update({"move_line_ids": move_line})
+
+        res.update(
+            {
+                "note": self.note,
+            }
+        )
 
         return res
 
     def _apply_inventory(self):
         res = super()._apply_inventory()
 
-        self.write({"note": self.note})
+        self.write({"note": False})
 
         return res
+
+    @api.model
+    def _get_inventory_fields_write(self):
+        res = super()._get_inventory_fields_write()
+        res.append("note")
+        return res
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    note = fields.Text()
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    note = fields.Text()
+    note = fields.Text(
+        related="move_id.note",
+    )
