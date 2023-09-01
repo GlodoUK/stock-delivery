a412b025ba28abe64415f7c1fa99c13f11bcc1d2
ellbristow <elliott@glo.systems>
2023-09-01 14:35:12 +0200

    Fix datetime formatting

--- delivery_parcelhub_whistl/models/delivery.py
+++ delivery_parcelhub_whistl/models/delivery.py
@@ -621,7 +621,7 @@ class DeliveryCarrier(models.Model):
                     self.env["stock.picking.tracking.history"].create(
                         {
                             "picking_id": picking.id,
-                            "date_event": datetime.strptime(
+                            "date_event": datetime.datetime.strptime(
                                 event.get("EventTimestamp"), "%Y-%m-%dT%H:%M:%S"
                             ),
                             "description": "%s>%s: (%s>%s) %s"
