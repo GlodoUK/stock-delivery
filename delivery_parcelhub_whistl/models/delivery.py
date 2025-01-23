5b3b4f10385314260686eae1681a584b9b9d08cc
Karl <karl@theangryangel.co.uk>
2025-01-23 17:29:12 +0000

    fix(delivery_parcelhub_whistl): missing reasonable time limit

--- delivery_parcelhub_whistl/models/delivery.py
+++ delivery_parcelhub_whistl/models/delivery.py
@@ -561,11 +561,12 @@ class DeliveryCarrier(models.Model):
                     "not in",
                     ["customer_delivered", "warehouse_delivered"],
                 ),
+                ("create_date", ">=", fields.Datetime.now() - datetime.timedelta(weeks=8)),
                 ("date_next_tracking_update", "<=", fields.datetime.now()),
                 "|",
                 ("carrier_consignment_ref", "!=", False),
                 ("carrier_tracking_ref", "!=", False),
-            ]
+            ], order="date_next_tracking_update ASC"
         )
 
     def whistl_tracking_state_update_scheduled(self):
